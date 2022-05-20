83caab1c5e3aa8e055827deb49c4c26fe0976b3d
Kolenkaa <kkolenkaa@gmail.com>
2022-05-20 21:29:37 +0300

    add solution 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,48 @@
+const fs = require('fs');
+const path = require('path');
 
+// fs.stat(path.join(__dirname, 'files-copy'), function (err) {
+// 	if (!err) {
+// 		console.log('Директория есть');		
+// 		removeFolder();		
+// 	}
+// 	else if (err.code === 'ENOENT') {
+// 		console.log('директории нет');	
+// 		createFolder();	
+// 	}
+// });
+
+async function removeFolder() {
+	try {
+		await fs.promises.rm(path.join(__dirname, 'files-copy'), {
+			recursive: true,
+			force: true
+		});
+	} catch (e) {
+		console.log(e);
+	}
+}
+
+async function createFolder() {
+	try {
+		await fs.promises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+	} catch (e) {
+		console.log(e);
+	}
+}
+createFolder();
+
+fs.readdir(path.join(__dirname, 'files'), (err, e) => {
+	if (err) {
+		throw err;
+	}
+	console.log('files copied:', e.length);
+	e.forEach(file => {
+		fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {
+			if (err) {
+				throw err;
+			}
+			console.log('copied', `${file}`);
+		})
+	})
+})
